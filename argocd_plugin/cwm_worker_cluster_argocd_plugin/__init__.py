7397a05791736891ccf39327033a5d57cc9c46af
Ori Hoch <ori@uumpa.com>
2024-01-27 15:41:16 +0200

    add argocd plugin implementation

--- argocd_plugin/cwm_worker_cluster_argocd_plugin/__init__.py
+++ argocd_plugin/cwm_worker_cluster_argocd_plugin/__init__.py
@@ -1,10 +1,6 @@
-import sys
-
-
 def init(chart_path):
     pass
 
 
 def generate_template(cmd, cwd, data):
-    print('generate_template', cmd, cwd, str(data), file=sys.stderr)
     return cmd, cwd
