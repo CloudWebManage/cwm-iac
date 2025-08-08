dcb32b579be073c7f0789998348625e3575dfdf8
Ori Hoch <ori@uumpa.com>
2025-08-08 09:56:03 +0300

    add local file tf module

---
+++ tfmodules/local_file/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    external = {
+      source = "hashicorp/external"
+    }
+  }
+}
+
+variable "bootstrap" {
+  type = bool
+  default = true
+}
+
+variable "command" {
+  type = string
+}
+
+variable "file_path" {
+  type = string
+  default = ""
+}
+
+variable "terraform_remote_state" {
+  type = object({
+    backend = string
+    config  = map(string)
+    output  = string
+  })
+  default = {
+    backend = ""
+    config  = {}
+    output  = ""
+  }
+}
+
+data "external" "fetch" {
+  count = var.bootstrap ? 1 : 0
+  program = [
+    "bash", "-c",
+    <<-EOT
+      set -euo pipefail
+      (
+        ${var.command}
+      ) | jq -Rs '{content: .}'
+    EOT
+  ]
+}
+
+data "terraform_remote_state" "content" {
+  count = var.bootstrap ? 0 : 1
+  backend = var.terraform_remote_state.backend
+  config = var.terraform_remote_state.config
+}
+
+resource "local_file" "content_bootstrap" {
+  filename = var.file_path
+  content = var.bootstrap ? data.external.fetch[0].result.content : data.terraform_remote_state.content[0].outputs[var.terraform_remote_state.output]
+}
+
+output "content" {
+  value = local_file.content_bootstrap.content
+}
