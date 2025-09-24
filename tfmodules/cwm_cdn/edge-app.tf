a15118b971e10cad8a2d4f5a7a7e97d5d6b994d9
Ori Hoch <ori@uumpa.com>
2025-09-24 21:30:24 +0300

    add openresty image

--- tfmodules/cwm_cdn/edge-app.tf
+++ tfmodules/cwm_cdn/edge-app.tf
@@ -2,4 +2,16 @@ module "edge-app" {
   source = "../argocd-app"
   name = "cdn-edge"
   create_namespace = false
+  values = var.versions["cwm-cdn-edge-openresty"] == "latest" ? null : {
+    cwmIac = {
+      openresty = {
+        image = "ghcr.io/cloudwebmanage/cwm-iac-openresty:${var.versions["cwm-cdn-edge-openresty"]}"
+      }
+    }
+  }
+  configSource = var.argocdConfigSource
+  configValueFiles = var.versions["cwm-cdn-edge-openresty"] == "latest" ? [
+    "config/auto-updated/cwm-iac/openresty.yaml"
+  ] : []
+  autosync = true
 }
