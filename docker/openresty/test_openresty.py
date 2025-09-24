beee3fd6cb6c4a259deb6822e5a5c749d062e7e8
Ori Hoch <ori@uumpa.com>
2025-09-24 20:18:18 +0300

    add openresty image

--- docker/openresty/test_openresty.py
+++ docker/openresty/test_openresty.py
@@ -35,7 +35,8 @@ def test():
     try:
         subprocess.check_call([
             "docker", "compose", "-f", "test_docker_compose.yaml",
-            "up", "--wait", "--yes", "--build", "--force-recreate", "--remove-orphans",
+            "up", "--wait", "--yes", "--force-recreate", "--remove-orphans",
+            *([] if os.getenv("CI") else ["--build"])
         ], cwd=os.path.join(os.path.dirname(__file__)))
         time.sleep(5)
         assert get_metrics() == {
