7cff1039f307cc9353088de6ae3921eef63e5f1b
Ori Hoch <ori@uumpa.com>
2025-12-07 19:01:01 +0200

    add minio log metrics

--- tfmodules/cwm_minio_tenant/env_config.tf
+++ tfmodules/cwm_minio_tenant/env_config.tf
@@ -13,7 +13,7 @@ resource "kubernetes_secret" "env-config" {
       export MINIO_STORAGE_CLASS_RRS="${var.erasure_code_reduced}"
       export MINIO_DOMAIN="${var.minio_domain}"
       export MINIO_AUDIT_WEBHOOK_QUEUE_DIR_METRICS="/export/audit-queue/metrics"
-      export MINIO_AUDIT_WEBHOOK_QUEUE_DIR_MAX_SIZE_METRICS="1000000"
+      export MINIO_AUDIT_WEBHOOK_QUEUE_SIZE_METRICS="1000000"
       export MINIO_AUDIT_WEBHOOK_ENABLE_METRICS="on"
       export MINIO_AUDIT_WEBHOOK_ENDPOINT_METRICS="http://localhost:8791"
     EOT
