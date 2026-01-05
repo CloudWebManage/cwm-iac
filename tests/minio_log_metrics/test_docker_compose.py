9208d45f5b8ca18db757fe44d67c44b8e091f42b
Ori Hoch <ori@uumpa.com>
2026-01-05 20:15:57 +0200

    fix the tests for the changes

--- tests/minio_log_metrics/test_docker_compose.py
+++ tests/minio_log_metrics/test_docker_compose.py
@@ -24,7 +24,7 @@ def test():
         dc("down", "-v")
     except subprocess.CalledProcessError:
         pass
-    dc("up", "-d", "--force-recreate", "--remove-orphans", "--wait")
+    dc("up", "-d", "--build", "--force-recreate", "--remove-orphans", "--wait")
     time.sleep(1)
     try:
         mc("alias", "set", "local", "http://localhost:9000", "admin", "12345678")
@@ -42,9 +42,12 @@ def test():
         for metric in text_string_to_metric_families(out):
             for sample in metric.samples:
                 if sample.labels.get("bucket") == "test":
-                    assert sample.name not in metric_values and sample.name not in metric_labels
-                    metric_values[sample.name] = sample.value
-                    metric_labels[sample.name] = sample.labels
+                    metric_name = sample.name
+                    if metric_name == "minio_audit_operations_total":
+                        metric_name = f'minio_audit_operations_total_{sample.labels["direction"]}'
+                    assert metric_name not in metric_values and metric_name not in metric_labels, f'duplicate metric samples found: {out}'
+                    metric_values[metric_name] = sample.value
+                    metric_labels[metric_name] = sample.labels
         assert metric_labels == {
             "minio_audit_bytes_received_total": {
                 "bucket": "test",
@@ -54,13 +57,20 @@ def test():
                 "bucket": "test",
                 "access_key": "admin",
             },
-            "minio_audit_operations_total": {
+            "minio_audit_operations_total_in": {
                 "bucket": "test",
                 "access_key": "admin",
+                "direction": "in",
+            },
+            "minio_audit_operations_total_out": {
+                "bucket": "test",
+                "access_key": "admin",
+                "direction": "out",
             }
         }
         assert metric_values["minio_audit_bytes_received_total"] > 30000000
         assert metric_values["minio_audit_bytes_sent_total"] > 60000000
-        assert metric_values["minio_audit_operations_total"] > 15
+        assert metric_values["minio_audit_operations_total_in"] > 13
+        assert metric_values["minio_audit_operations_total_out"] > 1
     finally:
-        pass  # dc("down", "-v")
+        dc("down", "-v")
