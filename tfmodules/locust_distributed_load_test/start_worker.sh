24cd0a60d4282bc4b449e206cc88387b467db1cd
Ori Hoch <ori@uumpa.com>
2026-02-04 22:10:31 +0200

    load tests

--- tfmodules/locust_distributed_load_test/start_worker.sh
+++ tfmodules/locust_distributed_load_test/start_worker.sh
@@ -5,8 +5,10 @@ docker rm -f locust || true
 
 systemctl restart locust-ssh-tunnel
 
+. $1
+
 docker run --name locust \
   -d \
   --env-file /root/locust.env --env-file $1 \
   ghcr.io/cloudwebmanage/cwm-minio-api-locust:latest \
-  --worker --master-host 172.17.0.1
+  --worker --master-host 172.17.0.1 $LOCUST_WORKER_ARGS
