23c7d0115c0875b049a87b7fb6a67e791a3637ec
Ori Hoch <ori@uumpa.com>
2026-03-26 11:38:30 +0200

    prometheus nagios checks sender add support for cluster instance

--- tfmodules/monitoring/prometheus_nagios_sender.tf
+++ tfmodules/monitoring/prometheus_nagios_sender.tf
@@ -17,5 +17,6 @@ resource "kubernetes_secret" "prometheus_nagios_sender_env" {
   data = {
     PROM_API_URL = "http://${module.monitoring_htpasswd.username}:${module.monitoring_htpasswd.password}@monitoring-kube-prometheus-prometheus:9090/api"
     SEND_NSCA_HOST = var.send_nsca_host
+    CLUSTER_INSTANCE_NAME = var.cluster_name
   }
 }
